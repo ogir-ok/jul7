57faa5612d2800c5da651a28ab408c3a8b3ec561
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2022-09-05 20:03:15 +0200

    db from amazon

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -15,3 +15,16 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'true').lower() in ('1', 'true')
 DEBUG = False
 
 ALLOWED_HOSTS = ['3.88.174.111', 'jul7.ogir-ok.com']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB', 'ithilleldb'),
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '123'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True
+    }
+}
